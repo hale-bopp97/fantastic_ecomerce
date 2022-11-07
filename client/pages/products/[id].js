9cbd8bd4eca9b71c5f093e756c65c4e18094c4fd
BT616 <110855674+BT616@users.noreply.github.com>
2022-11-07 13:47:26 -0800

    added images for products, started product details

--- client/pages/products/[id].js
+++ client/pages/products/[id].js
@@ -1,11 +1,18 @@
 import { useRouter } from "next/router"
+import Nav from '../../components/Nav'
+
+const productData = [
+    { name: "stickers", image: "/images/stickers.jpg", description: 'corgi sticker', price: "3.99" }]
+
 
 export default function ProductDetails() {
     const router = useRouter()
     const { id} = router.query
     return (
+        <div>
+            <Nav/>
       <h1 className="text-3xl font-bold underline">
       Product Details { id }
-      </h1>
+      </h1></div>
     )
   }
--- client/pages/products/index.js
+++ client/pages/products/index.js
@@ -1,12 +1,37 @@
+import Link from 'next/link'
 import Nav from '../../components/Nav'
+
+const productData = [
+    { name: "stickers", image: "/images/stickers.jpg" },
+    { name: "mousepad", image: '/images/mousepad.jpg' },
+    { name: 'keyboard', image: '/images/keyboard.jpg' },
+    { name: "mic", image: '/images/mic.jpg' },
+    { name: "monitor", image: '/images/monitor.jpg' },
+    { name: 'headphones', image: '/images/headphones.jpg' }]
+
 
 export default function Product() {
     return (
         <div>
-            <Nav/>
-      <h1 className="text-3xl font-bold underline">
-       Product Page
-      </h1>
-      </div>
+            <Nav />
+            <h1 className="text-center text-3xl font-bold p-5 ">
+                Products
+            </h1>
+            <section className='grid grid-cols-3 gap-10  justify-center px-10 py-10 content-center items-center'>
+                {productData.map((product) => {
+                    return <section>
+                        <Link href={`/products/${product.name}`}>
+                            <div class='bg-purple-300 grid grid-rows-2 justify-center content-center items-center text-center'>
+                                <img className='object-contain h-64 w-96' src={product.image}/>
+
+                                <p>{product.name}</p>
+
+                            </div>  
+                            </Link>
+                    </section>
+
+                })}
+            </section>
+        </div>
     )
-  }
+}
