5524ef25fc9898b69f50e9e1243cbab9f98afe13
BT616 <110855674+BT616@users.noreply.github.com>
2022-11-07 12:09:07 -0800

    install apollo, login page mockup

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,15 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import '../styles/globals.css'
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <ApolloProvider client={client}><Component {...pageProps} /></ApolloProvider>
 }
 
 export default MyApp
--- client/pages/login.js
+++ client/pages/login.js
@@ -2,11 +2,52 @@ import Nav from '../components/Nav'
 
 export default function Login() {
     return (
+
         <div>
             <Nav />
-            <h1 className="text-3xl font-bold underline">
-                Login Page
-            </h1>
+            <div className='h-screen grid justify-center px-10 py-10 content-center items-center grid-cols-2 gap-10 '>
+                <section>
+                    <h1 className="text-3xl font-bold">
+                        Login Page
+                    </h1>
+                    <form>
+                        <label class="block">
+                            <span className="block text-sm font-medium text-slate-700">Email</span>
+                            <input className="border-red-600 border-2 " />
+                        </label>
+
+                        <label class="block">
+                            <span className="block text-sm font-medium text-slate-700">Password</span>
+                            <input type="password" className="border-red-600 border-2 " />
+
+                        </label>
+                        <button>login</button>
+                    </form>
+                </section>
+
+                <section>
+                    <h1 className="text-3xl font-bold">Sign Up</h1>
+                    <form>
+
+                        <label class="block">
+                            <span className="block text-sm font-medium text-slate-700">User Name</span>
+                            <input className="border-red-600 border-2 " />
+                        </label>
+                        <label class="block">
+                            <span className="block text-sm font-medium text-slate-700">Email</span>
+                            <input className="border-red-600 border-2 " />
+                        </label>
+
+                        <label class="block">
+                            <span className="block text-sm font-medium text-slate-700">Password</span>
+                            <input type="password" className="border-red-600 border-2 " />
+
+                        </label>
+                        <button>Sign Up</button>
+                    </form>
+                </section>
+            </div>
         </div>
+
     )
 }
